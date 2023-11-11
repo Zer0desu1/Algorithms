f5d04aaf99c31d85cf7eec7d758ffe66c5354f8b
VURAL BİLGİN <90277329+Zer0desu1@users.noreply.github.com>
2023-11-11 13:16:56 +0300

    SelectionSort

---
+++ SelectionSort.cpp
@@ -0,0 +1,13 @@
+vector<int> selectionSort(vector<int>& arr) {
+    for (int i = 0; i < arr.size() - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < arr.size(); j++) {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
